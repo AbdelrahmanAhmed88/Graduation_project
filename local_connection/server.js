d7007797659ba871186bc77b62dfe16715a91a01
KhaledKonsowa88 <khaledkons@gmail.com>
2025-06-21 11:21:41 +0300

    adding local server

---
+++ local_connection/server.js
@@ -0,0 +1,36 @@
+const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const html = fs.readFileSync('index.html');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(html);
+});
+
+const wss = new WebSocket.Server({ server });
+
+let browserClient = null;
+
+wss.on('connection', (ws) => {
+  console.log('A client connected');
+
+  ws.on('message', (message) => {
+  const text = message.toString(); // <-- Add this line
+
+  console.log('Received:', text);
+
+  if (text === 'browser') {
+    browserClient = ws;
+  }
+
+  if (text === 'sleep' && browserClient) {
+    browserClient.send('sleep');
+  }
+});
+
+});
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080');
+});
